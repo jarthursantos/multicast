e042b2521117bd4e8786a502bdfcb82e170ce92e
Arthur Santos <silva.arthur@academico.ifpb.edu.br>
2020-09-17 16:07:42 -0300

    chore: use shared page in accounts client

--- packages/clients/accounts/src/pages/Auth/index.tsx
+++ packages/clients/accounts/src/pages/Auth/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
 import { Page } from '@shared/web-components'
+import { AuthPage } from '@shared/web-pages'
+
+import { version } from '../../../package.json'
 
 const Auth: React.FC = () => {
-  return <Page title="Entrar" />
+  return (
+    <Page title="Entrar">
+      <AuthPage version={version} />
+    </Page>
+  )
 }
 
 export default Auth
