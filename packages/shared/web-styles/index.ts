b296f4d8282ab92c6266a78f840b9d58656fd891
Arthur Santos <silva.arthur@academico.ifpb.edu.br>
2020-09-18 13:03:10 -0300

    feat: add shared global style

---
+++ packages/shared/web-styles/index.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  html,
+  body,
+  #root {
+    height: 100%
+  }
+`
