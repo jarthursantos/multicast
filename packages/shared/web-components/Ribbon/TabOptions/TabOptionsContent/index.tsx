0ea3594c4687dfb584b1aa7eae53ddfcb7bb98b3
Arthur Santos <silva.arthur@academico.ifpb.edu.br>
2020-09-30 08:52:27 -0300

    chore: add options content separator

--- packages/shared/web-components/Ribbon/TabOptions/TabOptionsContent/index.tsx
+++ packages/shared/web-components/Ribbon/TabOptions/TabOptionsContent/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 
 import { useActiveTab } from '../../Wrapper/context'
-import { Container } from './styles'
+import { Container, Separator } from './styles'
 import { TabOptionsContentProps } from './types'
 
-const TabOptionsContent: React.FC<TabOptionsContentProps> = ({
-  name,
-  children
-}) => {
+const TabOptionsContent: React.FC<TabOptionsContentProps> & {
+  Separator: typeof Separator
+} = ({ name, children }) => {
   const [isActive] = useActiveTab(name)
 
   return <Container hidden={!isActive}>{children}</Container>
 }
+
+TabOptionsContent.Separator = Separator
 
 export { TabOptionsContent }
--- packages/shared/web-components/Ribbon/TabOptions/TabOptionsContent/styles.ts
+++ packages/shared/web-components/Ribbon/TabOptions/TabOptionsContent/styles.ts
@@ -4,4 +4,15 @@ export const Container = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+  padding: 0 16px;
+
+  & > * + * {
+    margin-left: 12px;
+  }
+`
+
+export const Separator = styled.div`
+  height: 80px;
+  width: 1px;
+  background: ${({ theme }) => theme.colors.border.secondary};
 `
