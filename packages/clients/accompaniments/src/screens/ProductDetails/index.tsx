47dd5d18933d861c84e5cd0c81de5e89f87bfda9
Arthur Santos <silva.arthur@academico.ifpb.edu.br>
2021-02-19 14:25:49 -0300

    chore: accompaniments build 1.5

--- packages/clients/accompaniments/src/screens/ProductDetails/index.tsx
+++ packages/clients/accompaniments/src/screens/ProductDetails/index.tsx
@@ -1,15 +1,93 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Line } from 'react-chartjs-2'
 
+import { format } from 'date-fns'
+
+import { useAxios, extractErrorMessage } from '@shared/axios'
 import { formatPriceWithoutSymbol } from '@shared/web-components/DataGrid/Body/Row/Cell/Contabil/format'
 import { Form, TextInput, NumberInput } from '@shared/web-components/Form'
 
+import { normalizeDate } from '../Scredules/context'
 import { Wrapper, ChartWrapper, Inline, InlineFlexRight } from './styles'
-import { ProductDetailsScreenProps } from './types'
+import { ProductDetailsScreenProps, PriceHistory } from './types'
+
+function buildData(labels: string[], values: number[]) {
+  return {
+    labels,
+    datasets: [
+      {
+        label: 'Preço de Compra',
+        backgroundColor: 'rgba(51, 51, 51, 0.2)',
+        borderColor: 'rgba(51, 51, 51, 1)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(51, 51, 51, 0.4)',
+        hoverBorderColor: 'rgba(51, 51, 51, 1)',
+        data: values
+      }
+    ]
+  }
+}
+
+function buildOptions() {
+  return {
+    maintainAspectRatio: false,
+
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            // beginAtZero: true,
+            // min: 3.5,
+            // max: 6,
+            callback: function (value: number) {
+              return 'R$ ' + formatPriceWithoutSymbol(value)
+            }
+          }
+        }
+      ]
+    }
+  }
+}
 
 const ProductDetailsScreen: React.VFC<ProductDetailsScreenProps> = ({
   product
 }) => {
+  const [api, haveToken] = useAxios()
+
+  const [labels, setLabels] = useState<string[]>([])
+  const [values, setValues] = useState<number[]>([])
+  const [isLoadingPriceHistory, setLoadingPriceHistory] = useState(false)
+
+  useEffect(() => {
+    async function loadPriceHistory() {
+      setLoadingPriceHistory(true)
+
+      try {
+        const { data } = await api.get<PriceHistory[]>(
+          `/products/${product.code}/priceHistory`
+        )
+
+        setLabels(
+          data.map(({ entryAt }) =>
+            format(normalizeDate(entryAt), 'dd/MM/yyyy')
+          )
+        )
+
+        setValues(data.map(({ price }) => parseFloat(price.toFixed(2))))
+      } catch (error) {
+        const message = extractErrorMessage(error)
+
+        console.log(String(message))
+      } finally {
+        setLoadingPriceHistory(false)
+      }
+    }
+
+    if (haveToken && product) {
+      loadPriceHistory()
+    }
+  }, [api, product, haveToken])
+
   return (
     <Wrapper>
       <Form onSubmit={console.log} initialData={product}>
@@ -61,53 +139,10 @@ const ProductDetailsScreen: React.VFC<ProductDetailsScreenProps> = ({
       </Form>
 
       <ChartWrapper>
-        <Line
-          data={data}
-          options={{
-            maintainAspectRatio: false,
-
-            scales: {
-              yAxes: [
-                {
-                  ticks: {
-                    // beginAtZero: true,
-                    min: 3.5,
-                    max: 6,
-                    callback: function (value: number) {
-                      return 'R$ ' + formatPriceWithoutSymbol(value)
-                    }
-                  }
-                }
-              ]
-            }
-          }}
-        />
+        <Line data={buildData(labels, values)} options={buildOptions()} />
       </ChartWrapper>
     </Wrapper>
   )
-}
-
-const data = {
-  labels: [
-    'Jan, 2020',
-    'Set, 2020',
-    'Out, 2020',
-    'Nov, 2020',
-    'Dez, 2020',
-    'Jan, 2021',
-    'Fev, 2021'
-  ],
-  datasets: [
-    {
-      label: 'Preço de Compra',
-      backgroundColor: 'rgba(51, 51, 51, 0.2)',
-      borderColor: 'rgba(51, 51, 51, 1)',
-      borderWidth: 1,
-      hoverBackgroundColor: 'rgba(51, 51, 51, 0.4)',
-      hoverBorderColor: 'rgba(51, 51, 51, 1)',
-      data: [5.5, 5.6, 5.7, 5.4, 5.6, 5.5, 4.0]
-    }
-  ]
 }
 
 export { ProductDetailsScreen }
--- packages/clients/accompaniments/src/screens/ProductDetails/types.ts
+++ packages/clients/accompaniments/src/screens/ProductDetails/types.ts
@@ -1,5 +1,10 @@
 import { Product } from '~/windows/ProductDetails/types'
 
+export interface PriceHistory {
+  entryAt: Date | string
+  price: number
+}
+
 export interface ProductDetailsScreenProps {
   product: Product
 }
